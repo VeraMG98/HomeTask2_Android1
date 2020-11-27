3869bceec2cc70a32ec9ef8d3617b23b67c9fb4e
VeraMG98 <rusyamg@mail.ru>
2020-11-27 11:24:29 +0300

    Commit

--- app/src/main/java/com/example/hometask2_android1/MainActivity.java
+++ app/src/main/java/com/example/hometask2_android1/MainActivity.java
@@ -5,16 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText num_first, num_second, num_third, num_calculate;
-    Button button_result, button_second_result;
-    TextView textView_result, textView_second_result;
+    EditText num_first, num_second, num_third;
+    Button button_result;
+    TextView textView_result;
     String key;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(key, textView_result.getText().toString());
+        outState.putString(key, result + "");
     }
 
     @Override
@@ -41,11 +43,8 @@ public class MainActivity extends AppCompatActivity {
         num_first = findViewById(R.id.first_num);
         num_second = findViewById(R.id.second_num);
         num_third = findViewById(R.id.third_num);
-        num_calculate = findViewById(R.id.calculate_edit);
         textView_result = findViewById(R.id.result);
-        textView_second_result = findViewById(R.id.second_result);
         button_result = findViewById(R.id.button_sum);
-        button_second_result = findViewById(R.id.button_result);
 
         button_result.setOnClickListener(v -> {
             if (TextUtils.isEmpty(num_first.getText().toString())
@@ -53,10 +52,9 @@ public class MainActivity extends AppCompatActivity {
                     || TextUtils.isEmpty(num_third.getText().toString())) {
                 return;
             }
-            int result = Integer.parseInt(num_first.getText().toString())
+            result = (Integer.parseInt(num_first.getText().toString())
                     + Integer.parseInt(num_second.getText().toString())
-                    + Integer.parseInt(num_third.getText().toString());
-            textView_result.setText( "Result: " + result);
+                    + Integer.parseInt(num_third.getText().toString()));
         });
     }
 }
